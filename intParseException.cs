289a1f0b73f015b40e379db3aa2240b7e6d1cfde
Yan <77746251+yanxu2021@users.noreply.github.com>
2021-02-05 19:52:15 -0800

    calls int.parse("17") and handle the exception
    //Write a snippet of code which:
    //calls int.parse("17")
    //handles the appropriate exception if thrown
    //outputs “Done!” to the screen regardless of whether an exception is thrown

---
+++ intParseException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+//Write a snippet of code which:
+//calls int.parse("17")
+//handles the appropriate exception if thrown
+//outputs “Done!” to the screen regardless of whether an exception is thrown
+
+
+namespace intParseException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int numVal = Int32.Parse("17");//Convert a string representation of number to an integer.
+                Console.WriteLine(numVal);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Done!");
+            }
+        }
+    }
+}
