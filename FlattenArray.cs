6d2b5a84478eb41e2edfdce83c7ba61e83a4267d
Yan <77746251+yanxu2021@users.noreply.github.com>
2021-02-13 15:31:25 -0800

    FlattenArray
    How do you “flatten” a multidimensional array? In other words, take something that looks like a matrix  [{1,2,3},{4,5,6},{7,8,9}]and turns it into an array  [ 1 2 3 4 5 6 7 8 9]?

---
+++ FlattenArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+//How do you “flatten” a multidimensional array? 
+//A squared matrix like
+/*
+1 2 3
+4 5 6
+7 8 9
+*/
+
+//Flatten into [1 2 3 4 5 6 7 8 9]
+
+namespace FlattenArray
+{
+
+    class Program
+    {
+
+        static void Main()
+        {
+            int[,] array2D = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+                       
+            var rows = array2D.GetLength(0);
+            var cols = array2D.GetLength(1);
+            var array1D = new int[rows * cols];
+            var current = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    array1D[current++] = array2D[i, j];
+                }
+            }
+
+            Console.WriteLine("[{0}]", string.Join(", ",array1D));
+        }
+    }
+}
+
+
+//Output:[1, 2, 3, 4, 5, 6, 7, 8, 9]
