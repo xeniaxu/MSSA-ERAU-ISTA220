b7bd3f9ec16f3f4e469dfaceb0e2702673ee06f6
Yan <77746251+yanxu2021@users.noreply.github.com>
2021-03-26 11:23:59 -0700

    Add files via upload

---
+++ TupleNote.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+//Note Date:2021-3-26
+//Review Date:2021-3-26
+
+//定义：A tuple is a data structure that contains a sequence of elements of different data types.(可以理解为一种简化class)
+//It can be used where you want to have a data structure to hold an object with properties, but you don't want to create a separate type for it.
+
+//语法：Tuple<T1, T2, T3, T4, T5, T6, T7, TRest>
+//A tuple can only include a maximum of 8 elements. It gives a compiler error when you try to include more than 8 elements.
+//If you want to include more than eight elements in a tuple, you can do that by nesting another tuple object as the eighth element.
+//The last nested tuple can be accessed using the Rest property.
+//To access the nested tuple's element, use the Rest.Item1.Item<elelementNumber> property.
+
+//应用：A tuple elements can be accessed with Item<elementNumber> properties, e.g., Item1, Item2, Item3, and so on up to Item7 property.
+//The Item1 property returns the first element, Item2 returns the second element, and so on.
+//The last element (the 8th element) will be returned using the Rest property.
+//Generally, the 8th position is for the nested tuple, which you can access using the Rest property.
+
+//小结：Tuples can be used in the following scenarios:
+//1. When you want to return multiple values from a method without using ref or out parameters.
+//2. When you want to pass multiple values to a method through a single parameter.
+//3. When you want to hold a database record or some values temporarily without creating a separate class.
+
+//Limitations:
+//The Tuple is a reference type and not a value type. It allocates on heap and could result in CPU intensive operations.
+//The Tuple is limited to include eight elements. You need to use nested tuples if you need to store more elements. However, this may result in ambiguity.
+//The Tuple elements can be accessed using properties with a name pattern Item<elementNumber>, which does not make sense.
+
+namespace TupleNote
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var person = Tuple.Create(1, "Steve", "Jobs");
+            Console.WriteLine(person.Item1); // returns 1
+			Console.WriteLine(person.Item2); // returns "Steve"
+			Console.WriteLine(person.Item3); // returns "Jobs"
+
+
+			var numbers = Tuple.Create("One", 2, 3, "Four", 5, "Six", 7, 8);
+			Console.WriteLine(numbers.Item1); // returns "One"
+			Console.WriteLine(numbers.Item2); // returns 2
+			Console.WriteLine(numbers.Item3); // returns 3
+			Console.WriteLine(numbers.Item4); // returns "Four"
+			Console.WriteLine(numbers.Item5); // returns 5
+			Console.WriteLine(numbers.Item6); // returns "Six"
+			Console.WriteLine(numbers.Item7); // returns 7
+			Console.WriteLine(numbers.Rest); // returns (8)
+			Console.WriteLine(numbers.Rest.Item1); // returns 8
+
+			var numbers2 = Tuple.Create(1, 2, 3, 4, 5, 6, 7, Tuple.Create(8, 9, 10, 11, 12, 13));
+			Console.WriteLine(numbers2.Item1); // returns 1
+			Console.WriteLine(numbers2.Item7); // returns 7
+			Console.WriteLine(numbers2.Rest.Item1); //returns (8, 9, 10, 11, 12, 13)
+			Console.WriteLine(numbers2.Rest.Item1.Item1); //returns 8
+			Console.WriteLine(numbers2.Rest.Item1.Item2); //returns 9
+
+			//实际上不一定嵌套第八位，You can include the nested tuple object anywhere in the sequence.
+			//但是，However, it is recommended to place the nested tuple at the end of the sequence so that it can be accessed using the Rest property.
+
+			//A method can have a tuple as a parameter.
+			var classmates = Tuple.Create(1, "Steve", "Jobs");
+			DisplayTuple(classmates);
+
+			//A Tuple can be return from a method.
+			var colleague = GetColleague();
+			Console.WriteLine(colleague);//(1, Bill, Gates)
+		}
+
+		static void DisplayTuple(Tuple<int, string, string> classmates)
+		{
+			Console.WriteLine($"Id = { classmates.Item1}");//Id = 1
+			Console.WriteLine($"First Name = { classmates.Item2}");//First Name = Steve
+			Console.WriteLine($"Last Name = { classmates.Item3}");//Last Name = Jobs
+		}
+
+		static Tuple<int, string, string> GetColleague()
+		{
+			return Tuple.Create(1, "Bill", "Gates");
+		}
+	}
+}
---
+++ ValueTupleNote.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+//Note Date:2021-3-26
+//Review Date:2021-3-26
+
+//定义：C# 7.0 (.NET Framework 4.7) introduced the ValueTuple structure, which is a value type representation of the Tuple(Reference type).
+//It is easy to create and initialize the ValueTuple. It can be created and initialized using parentheses () and specifying the values in it.
+
+//安装：To install the ValueTuple package, right-click on the project in the solution explorer and select Manage NuGet Packages, this will open the NuGet Package Manager.
+//Click the Browse tab, search for ValueTuple in the search box, and select the System.ValueTuple package.
+
+namespace ValueTupleNote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = (1, "Bill", "Gates");
+            //equivalent Tuple
+            //var person = Tuple.Create(1, "Bill", "Gates");
+            Console.WriteLine(person);
+
+            //The ValueTuple can also be initialized by specifying the type of each element.
+            (int, string, string) classmates = (100, "yan", "xu");//Please notice that we have not used var in the above tuple initialization statement;
+                                                                  //instead, we provided the type of each member values inside the brackets.
+            Console.WriteLine(classmates.Item1);  //100
+            Console.WriteLine(classmates.Item2);   //yan
+            Console.WriteLine(classmates.Item3);   //xu
+
+            //Tuple requires at least two values.
+            var number = (1);  // int type, NOT a tuple
+            var numbers = (1, 2); //valid tuple
+            Console.WriteLine(Convert.ToString(number));//1
+            Console.WriteLine(numbers);//(1, 2)
+
+            //Unlike Tuple, a ValueTuple can include more than eight values.
+            var numbers2 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
+            Console.WriteLine(numbers2);
+
+            //Assign names to the ValueTuple properties instead of having the default property names like Item1, Item2 and so on.
+            (int Id, string FirstName, string LastName) famous = (1, "Bill", "Gates");
+            Console.WriteLine(famous.Id);   // 1
+            Console.WriteLine(famous.FirstName);  //Bill
+            Console.WriteLine(famous.LastName); // Gates
+
+            //Assign member names on the right side with values.
+            var famous2 = (Id: 1, FirstName: "Bill", LastName: "Gates");
+            Console.WriteLine(famous2);//(1, Bill, Gates)
+            //Please note that we can provide member names either on the left or right sides but not on both sides.
+            //The left side has precedence over the right side. 
+
+            //We can also assign variables as member values.
+            string firstName = "Bill", lastName = "Gates";
+            var per = (FirstName: firstName, LastName: lastName);
+            Console.WriteLine(per);//(Bill, Gates)
+
+            //ValueTuple as Return Type.
+            DisplayTuple(person);//Id = 1
+                                 //First Name = Bill
+                                 //Last Name = Gates
+
+            //Specify different member names for a ValueTuple returned from a method.
+            var person2 = GetPerson();
+            Console.WriteLine(person2.Id);//1
+            Console.WriteLine(person2.FirstName);//Bill
+            Console.WriteLine(person2.LastName);//Gates
+
+            //Individual members of a ValueTuple can be retrieved by deconstructing it.ValueTuple的单个成员可通过解构获得。
+            //The general syntax for deconstructing a tuple is similar to the syntax for defining one:
+            //You enclose the variables to which each element is to be assigned in parentheses in the left side of an assignment statement.
+            //为什么解构？A tuple provides a lightweight way to retrieve multiple values from a method call.
+            //But once you retrieve the tuple, you have to handle its individual elements.->很麻烦
+            //A single deconstruct operation可以解决这个问题。
+
+            //There are three ways to deconstruct a tuple:
+            //1.  Explicitly declare the type of each field inside parentheses. 
+            (string city, int population, double area) = QueryCityData("New York City");
+            Console.WriteLine(city);//New York City
+            Console.WriteLine(population);//8175133
+            Console.WriteLine(area);//468.48
+
+            //2. Use the var keyword to infer the type of each variable->Place the var keyword outside of the parentheses.
+            var (city2, population2, area2) = QueryCityData("New York City");
+            Console.WriteLine(city2);//New York City
+            Console.WriteLine(population2);//8175133
+            Console.WriteLine(area2);//468.48
+
+            //累赘笨重不推荐，3. Use the var keyword individually with any or all of the variable declarations inside the parentheses.
+            (string city3, var population3, var area3) = QueryCityData("New York City");
+            Console.WriteLine(city3);//New York City
+            Console.WriteLine(population3);//8175133
+            Console.WriteLine(area3);//468.48
+
+            //Lastly, we may deconstruct the tuple into variables that have already been declared.
+            string city4 = "Raleigh";
+            int population4 = 458880;
+            double area4 = 144.8;
+
+            (city4, population4, area4) = QueryCityData("New York City");
+            Console.WriteLine(city4);//New York City
+            Console.WriteLine(population4);//8175133
+            Console.WriteLine(area4);//468.48
+
+            //Deconstructing tuple elements with discards.
+            var (_, _, _, pop1, _, pop2) = QueryCityDataForYears("New York City", 1960, 2010);
+            Console.WriteLine($"Population change, 1960 to 2010: {pop2 - pop1:N0}");//只选取两个人口数量作进一步操作。Population change, 1960 to 2010: 393,149
+
+        }
+
+        static void DisplayTuple((int, string, string) person)
+        {
+            Console.WriteLine($"Id = { person.Item1}");
+            Console.WriteLine($"First Name = { person.Item2}");
+            Console.WriteLine($"Last Name = { person.Item3}");
+        }
+
+        static (int Id, string FirstName, string LastName) GetPerson()
+        {
+            return (1, "Bill", "Gates");
+        }
+
+        private static (string, int, double) QueryCityData(string name)
+        {
+            if (name == "New York City")
+                return (name, 8175133, 468.48);
+
+            return ("", 0, 0);
+        }
+
+        private static (string, double, int, int, int, int) QueryCityDataForYears(string name, int year1, int year2)
+        {
+            int population1 = 0, population2 = 0;
+            double area = 0;
+
+            if (name == "New York City")
+            {
+                area = 468.48;
+                if (year1 == 1960)
+                {
+                    population1 = 7781984;
+                }
+                if (year2 == 2010)
+                {
+                    population2 = 8175133;
+                }
+                return (name, area, year1, population1, year2, population2);
+            }
+
+            return ("", 0, 0, 0, 0, 0);
+        }
+    }
+}
